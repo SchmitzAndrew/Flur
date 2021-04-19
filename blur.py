463423e0fc5fcda4b8f27c68686687de448e8197
SchmitzAndrew <schmitzandrew03@gmail.com>
2021-04-19 10:56:28 -0700

    blur code

--- blur.py
+++ blur.py
@@ -1,5 +1,12 @@
-from PIL import Image
+from PIL import Image, ImageFilter
+import numpy
 
-def blur(img, part):
+def blur(img, part, intensity):
+    im = Image.open(img)
+
     for x, y, w, h in part:
-
+        box = (x, y, x + w, y + h)
+        region = im.crop(box)
+        blurred = region.filter(ImageFilter.GaussianBlur(intensity))
+        im.paste(blurred, im)
+    im.save()
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import cv2
 
+
 from imgDetection import Img
 from vidDetection import Vid
+from blur import blur
 
 #Cascades
 faceCascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt.xml")
 eyeCascade = cv2.CascadeClassifier("cascades/haarcascade_eye.xml")
 
-faceImg = Img("images/lcd_soundsystem.jpg", faceCascade)
+faceImg = Img("images/daft_punk_unmasked.jpg", faceCascade)
+faces = faceImg.detect_face()
+img = faceImg.draw_rectangle(faces)
+blur(img, faces, 5)
+faceImg.show_img(img)
 
-# faces = faceImg.detect_face()
-# img = faceImg.draw_rectangle(faces)
-# faceImg.show_img(img)
-
-faceVid = Vid(faceCascade, eyeCascade)
-faceVid.capture_video()
+# faceVid = Vid(faceCascade, eyeCascade)
+# faceVid.capture_video()
 
